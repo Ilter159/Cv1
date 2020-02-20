ab179983c379ff187c0932dfc375a99215f553de
Ilter159 <asperisyan@hotmail.com>
2020-02-20 11:02:30 +0300

    Cv Example Web Site

---
+++ Cv/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Giriş Ekranınna Hoş Geldiniz";
+            return View();
+        }
+        public ActionResult Cv1() 
+        {
+            return View();
+        }
+        public ActionResult Cv2()
+        {
+            return View();
+        }
+    }
+}
